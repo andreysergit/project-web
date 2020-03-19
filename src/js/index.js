2b7e40e2f4e065872db0b4d3ac8f3ff1708c6b6b
andreolecture <andreysergit@gmail.com>
2020-03-19 15:31:42 +0300

    second commit

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,21 @@
-let age;
-age = prompt ('your age?');
+// let message;
+// let age = prompt('Сколько вам лет?');
+// if (age < 18) {
+//   message = 'Привет';
+// } else if (age === 18) {
+//   message = 'Здравствуй';
+// } else {
+//   message = 'Здравствуйте';
+// }
+// console.log(message)
 
-const grovCondition = age>=18;
-
-if (grovCondition) {
-  const message = "access.permited";
-  console.log(message);
-} else {
-  const message = "access.denied";
-  console.log(message);
+let a = 10;
+switch (a) {
+  case a > 10:
+    console.log('Попробуйте еще раз');
+    break;
+  case a === 10:
+    console.log('Правильно')
+  default:
+    console.log(a)
 }
