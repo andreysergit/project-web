9f22d65d4569f5dfe109d33d97ed862eff6def7d
andreolecture <andreysergit@gmail.com>
2020-03-20 16:18:36 +0300

    Homework

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,5 @@
+// Условия if----------
+
 // let message;
 // let age = prompt('Сколько вам лет?');
 // if (age < 18) {
@@ -9,13 +11,41 @@
 // }
 // console.log(message)
 
-let a = 10;
-switch (a) {
-  case a > 10:
-    console.log('Попробуйте еще раз');
-    break;
-  case a === 10:
-    console.log('Правильно')
-  default:
-    console.log(a)
+// switch--------
+
+// let a = 10;
+// switch (a) {
+//   case a > 10:
+//     console.log('Попробуйте еще раз');
+//     break;
+//   case a === 10:
+//     console.log('Правильно')
+//   default:
+//     console.log(a)
+// }
+
+// Task 'for'--------
+
+// for (i = -9; i <= -5; ++i) {
+//   console.log(i + 9);
+//   console.log(i);
+//   if (!i) break;
+// }
+
+// Task
+// Implement function, that for given two numbers, presented as strings will return their product ----------
+
+// const str1 = '2', str2 = '2';
+// num1 = Number.parseInt(str1);
+// num2 = Number.parseInt(str2);
+// console.log(num1 * num2);
+
+function zeros() {
+  let num1 = '5!';
+  let str1= Number.parseInt(num1);
+  console.log(str1);
 }
+
+zeros();
+
+
