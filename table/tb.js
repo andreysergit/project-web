bd75ee84c862fb90062819ae2b712e21b47d9a82
andreysergit <andreysergit@gmail.com>
2020-04-05 19:44:57 +0300

    homework DOM

---
+++ table/tb.js
@@ -0,0 +1,36 @@
+"use strict";
+
+let data = [
+  {
+    firstName: "Ashton",
+    lastName: "Kutcher",
+    age: 40,
+  },
+  {
+    firstName: "Bradley",
+    lastName: "Pitt",
+    age: 54,
+  },
+  {
+    firstName: "Hannah",
+    lastName: "Dakota",
+    age: 24,
+  },
+];
+
+let table = document.getElementById("table");
+
+document.body.appendChild(table);
+let i = 1;
+
+data.forEach((elem) => {
+  let tr = document.createElement("tr");
+  tr.appendChild(document.createElement("td"));
+  tr.appendChild(document.createElement("td"));
+  tr.appendChild(document.createElement("td"));
+  table.appendChild(tr);
+  table.rows[i].cells[0].innerText = elem.firstName;
+  table.rows[i].cells[1].innerText = elem.lastName;
+  table.rows[i].cells[2].innerHTML = elem.age;
+  i++;
+});
